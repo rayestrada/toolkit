d2853a59af24d592805f45f8e997c7f16e6499d3
Ray Estrada <ray@trestlemedia.net>
2016-12-01 15:58:49 -0800

    Accessibility Update to carousel module

--- src/js/init-carousel.js
+++ src/js/init-carousel.js
@@ -5,7 +5,8 @@ require('owl.carousel/dist/assets/owl.carousel.min.css');
 $(function (){
   // Owl Carousel
   // Config options here http://www.owlcarousel.owlgraphic.com/docs/api-options.html
-  $('.owl-carousel').owlCarousel({
+  var owl = $('.owl-carousel');
+  owl.owlCarousel({
     items: 1,
     loop: true,
     autoplay: true,
@@ -13,6 +14,22 @@ $(function (){
     autoplaySpeed: 1000,
     autoplayHoverPause: true,
     nav: true,
+    navText: ['<a href="#" class="pd">next slide</a>', '<a href="#" class="pd">previous slide</a>'],
     dots: false
+  }).after('<div class="owl-autoplay-controls"><a href="#" class="owl-start pd">Start</a><a href="#" class="owl-stop pd">Stop</a></div>');
+
+  // Prevent default link interaction on carousel controls
+  $('a.pd').on('click', function(e){
+    e.preventDefault();
+  });
+
+  // trigger start
+  $('.owl-start').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+  });
+
+  // trigger stop
+  $('.owl-stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
   });
 });
