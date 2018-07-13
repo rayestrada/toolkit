411922b9d0b72b001468149250b8c5714a97e299
Ray Estrada <ray.estrada@agencychief.com>
2018-07-13 13:06:16 -0700

    Update Gulp to v4 Update Webpack to 3 Update toolkit to use local gulp instead of global Update compatibility with node 8

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,7 @@
 
 var path = require('path');
 var webpack = require('webpack');
-var jQuery = require('jquery');
 var glob = require('glob');
-
-var glob_entries = function (globs) {
-  var entries = {};
-  Object.keys(globs).forEach(function (key) {
-    var globPath = globs[key];
-    var files = glob.sync(globPath);
-
-    for (var i = 0; i < files.length; i++) {
-      var entry = files[i];
-      entries[key + '/' + path.basename(entry, path.extname(entry))] = entry;
-    }
-  });
-  return entries;
-};
 
 module.exports = function (gulpConfig) {
   var config = {
@@ -31,7 +16,7 @@ module.exports = function (gulpConfig) {
       'jquery': 'jQuery'
     },
     module: {
-      loaders: [
+      rules: [
         {
           // Used for loading css files from modules using js
           test: /\.css$/,
@@ -45,22 +30,32 @@ module.exports = function (gulpConfig) {
           loader: 'url-loader?limit=20000' // (20kb) Any file smaller than limit will use data uri instead
         }
       ]
-    },
-    plugins: [
-      // loads jquery within the context of the files and provides it as a global variable for files
-      // new webpack.ProvidePlugin({
-      //   '$':'jquery',
-      //   'jQuery':'jquery',
-      //   'window.jQuery':'jquery'
-      // })
-    ]
-  }
+    }
+  };
 
   if (!gulpConfig.dev) {
-    config.plugins.push(
-      new webpack.optimize.UglifyJsPlugin()
-    );
+    new webpack.optimize.UglifyJsPlugin();
   }
 
   return config;
+};
+
+
+/**
+ * A function to glob entry points to achieve separate output files
+ * @param globs
+ * @returns {{}}
+ */
+var glob_entries = function (globs) {
+  var entries = {};
+  Object.keys(globs).forEach(function (key) {
+    var globPath = globs[key];
+    var files = glob.sync(globPath);
+
+    for (var i = 0; i < files.length; i++) {
+      var entry = files[i];
+      entries[key + '/' + path.basename(entry, path.extname(entry))] = entry;
+    }
+  });
+  return entries;
 };
